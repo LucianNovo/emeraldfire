af0c459747b80482e62b849e442776c572bb91c3
Thomas Boyt <me@thomasboyt.com>
2013-04-09 14:44:35 -0400

    Use ENV['DB_NAME'] for database name

--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -3,6 +3,6 @@
 EmberFirebaseChat.store = DS.Store.create({
   revision: 12,
   adapter: DS.Firebase.Adapter.create({
-    dbName: "emberchat"
+    dbName: window.DB_NAME  // populated by <script> tag in rails application view
   })
 });
