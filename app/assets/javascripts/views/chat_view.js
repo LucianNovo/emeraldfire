b2f1fd25af34f0f5b6df9f22f4aa73c17168f254
Thomas Boyt <me@thomasboyt.com>
2013-04-13 15:17:59 -0400

    Added autoscroll

--- app/assets/javascripts/views/chat_view.js
+++ app/assets/javascripts/views/chat_view.js
@@ -1,3 +1,13 @@
 EmberFirebaseChat.ChatView = Ember.View.extend({
+  autoScroll: function() {
+    if (this.state == "inDOM") {
+      // Ember.run.next waits until the DOM has been updated :)
+      Ember.run.next(this, function() {
+        var container = this.$(".chat-messages").get(0);
+        console.log(container.scrollHeight);
+        container.scrollTop = container.scrollHeight;
+      });
+    }
+  }.observes("controller.content.length"),
 });
 
