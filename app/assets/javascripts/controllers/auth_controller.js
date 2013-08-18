bfcafb14478439ec14f333726f435d7c721b0fd2
Lucian Novocel <lnovocel@Timothys-13-MacBook-Pro.local>
2013-08-18 03:22:17 -0700

    removed some references to FireBaseChat, now replacing them with EmberpressFirebase replacements, and modeling the board and playre model after what was provided by emberpress.

--- app/assets/javascripts/controllers/auth_controller.js
+++ app/assets/javascripts/controllers/auth_controller.js
@@ -1,32 +1,32 @@
-EmberFirebaseChat.AuthController = Ember.Controller.extend({
-  authed: false,
-  hasName: false,
+// EmberFirebaseChat.AuthController = Ember.Controller.extend({
+//   authed: false,
+//   hasName: false,
 
-  init: function() {
-    this.authClient = new FirebaseAuthClient(EmberFirebaseChat.store.adapter.fb, function(err, user) {
-      if (!err) {
-        if (user) {
-          this.set("user", EmberFirebaseChat.User.find(user.id));
+//   init: function() {
+//     this.authClient = new FirebaseAuthClient(EmberFirebaseChat.store.adapter.fb, function(err, user) {
+//       if (!err) {
+//         if (user) {
+//           this.set("user", EmberFirebaseChat.User.find(user.id));
 
-          var user = this.get("user");
-          user.one("didLoad", function() {
-            if (user.get("nickname")) {
-              this.set("hasName", true);
-            }
-            this.set("authed", true);
-          }.bind(this));
-        }
-      }
-    }.bind(this));
-  },
+//           var user = this.get("user");
+//           user.one("didLoad", function() {
+//             if (user.get("nickname")) {
+//               this.set("hasName", true);
+//             }
+//             this.set("authed", true);
+//           }.bind(this));
+//         }
+//       }
+//     }.bind(this));
+//   },
 
-  pickName: function(name) {
-    this.get("user").set("nickname", name);
-    EmberFirebaseChat.store.commit();
-    this.set("hasName", true);
-  },
+//   pickName: function(name) {
+//     this.get("user").set("nickname", name);
+//     EmberFirebaseChat.store.commit();
+//     this.set("hasName", true);
+//   },
 
-  login: function() {
-    this.authClient.login("persona");
-  }
-});
+//   login: function() {
+//     this.authClient.login("persona");
+//   }
+// });
